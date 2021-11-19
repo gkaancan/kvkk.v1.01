326bf7222a42915c0e6b47f453a04b1697aed080
Göktuğ <kaancan-9@hotmail.com>
2021-11-19 21:45:46 +0300

    Add procfile , update port

--- app.js
+++ app.js
@@ -678,7 +678,17 @@ app.get("/documents/:arg",function(req,res){
   }
 });
 
-app.listen(3000,function()
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+
+
+
+app.listen(port,function()
 {
   console.log("Server started on port 3000");
 });
